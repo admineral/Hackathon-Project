a91fea3739270248cb773c6edc6f194c2cb636bc
admineral <50579369+admineral@users.noreply.github.com>
2023-12-09 23:34:08 +0100

    changed size of BH and Orbits

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ import OrbitAnimation from './Orbit_Components/OrbitAnimation';
 
 export default function Home() {
   // Define multiple orbit sizes with increased inner orbit size
-  const orbits = [100, 150, 230]; // Updated radii of the orbits with more space for the inner circle
+  const orbits = [80, 140, 200]; // Updated radii of the orbits with more space for the inner circle
 
   return (
     <>
       <Header />
       <div className="flex justify-center items-center h-screen">
         {/* CentralCircle representing the black hole */}
-        <div className="relative w-32 h-32 bg-black rounded-full" style={{ margin: 'auto' }}>
+        <div className="relative w-16 h-16 bg-black rounded-full" style={{ margin: 'auto' }}>
           <OrbitAnimation orbits={orbits} />
         </div>
       </div>
