c5ad303c994f084c273208aaecf55a7435d53ff1
admineral <50579369+admineral@users.noreply.github.com>
2023-12-09 23:24:08 +0100

    new Text

--- app/LandingPage_Components/Header.js
+++ app/LandingPage_Components/Header.js
@@ -8,13 +8,13 @@ export default function Header() {
         className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
       >
-        Innovating Journalism
+        Community Voice
       </h1>
       <p
         className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
         style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
       >
-        Shaping the future of the newspaper industry in the digital age.
+        Reinventing the future of journalism through community engagement in the digital age.
       </p>
     </div>
   );
