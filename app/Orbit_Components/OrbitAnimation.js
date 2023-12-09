15a13876c377364e8ab2c9e109293a1830b600cd
admineral <50579369+admineral@users.noreply.github.com>
2023-12-10 00:04:32 +0100

    random orbit start position

--- app/Orbit_Components/OrbitAnimation.js
+++ app/Orbit_Components/OrbitAnimation.js
@@ -1,20 +1,14 @@
-// OrbitAnimation.js
 "use client";
 import { motion } from 'framer-motion';
 import React from 'react';
 
 export default function OrbitAnimation({ orbits }) {
-    
-  // Calculate the orbit path using sine and cosine for x and y
-  // Added an offset parameter to shift the starting position of the animation
   const calculateOrbitPath = (angle, size, offset = 0) => ({
     x: Math.cos(angle + offset) * size,
     y: Math.sin(angle + offset) * size
   });
 
-  // Use Framer Motion's keyframes to animate along each orbit path
   const orbitAnimations = orbits.map((orbitSize, index) => {
-    // Calculate an offset for each orbit based on its index. This will ensure each orbit starts from a different position.
     const offset = (index * 2 * Math.PI) / orbits.length;
     return {
       x: Array.from({ length: 360 / 10 }, (_, i) => calculateOrbitPath((i * 10) * Math.PI / 180, orbitSize, offset).x),
@@ -23,44 +17,51 @@ export default function OrbitAnimation({ orbits }) {
         repeat: Infinity, 
         repeatType: "loop", 
         duration: orbitSize * 0.4, 
-        ease: "linear"
+        ease: "linear",
+        delay: index * 0.5
       }
     };
   });
 
   return (
-    <>
-      {/* Visual representation of orbit paths */}
-      {orbits.map((size, index) => (
-        <div
-          key={index}
-          className="absolute border border-gray-300 rounded-full"
-          style={{
-            width: size * 2,
-            height: size * 2,
-            top: '50%',
-            left: '50%',
-            transform: `translate(-50%, -50%)`,
-            zIndex: 0,
-          }}
-        />
-      ))}
-      
-      {/* Orbiting objects */}
-      {orbitAnimations.map((animation, index) => (
-        <motion.div
-          key={index}
-          className="absolute w-8 h-8 bg-blue-500 rounded-full"
-          style={{
-            top: '50%',
-            left: '50%',
-            translateX: '-50%',
-            translateY: '-50%',
-            zIndex: 1,
-          }}
-          animate={animation}
-        />
-      ))}
-    </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 2 }}
+    >
+      <div className="relative w-16 h-16 bg-black rounded-full" style={{ margin: 'auto' }}>
+        {orbits.map((size, index) => (
+          <div
+            key={index}
+            className="absolute border border-gray-300 rounded-full"
+            style={{
+              width: size * 2,
+              height: size * 2,
+              top: '50%',
+              left: '50%',
+              transform: `translate(-50%, -50%)`,
+              zIndex: 0,
+            }}
+          />
+        ))}
+        
+        {orbitAnimations.map((animation, index) => (
+          <motion.div
+            key={index}
+            className="absolute w-8 h-8 bg-blue-500 rounded-full"
+            style={{
+              top: '50%',
+              left: '50%',
+              translateX: '-50%',
+              translateY: '-50%',
+              zIndex: 1,
+            }}
+            initial={{ x: animation.x[0], y: animation.y[0] }}
+            animate={animation}
+            transition={{ delay: 0.5, duration: 1.5 }} // Moved the transition prop here
+          />
+        ))}
+      </div>
+    </motion.div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
 // Home.js
-
-
 import React from 'react';
 import Header from './LandingPage_Components/Header';
 import OrbitAnimation from './Orbit_Components/OrbitAnimation';
 
 export default function Home() {
   // Define multiple orbit sizes with increased inner orbit size
-  const orbits = [80, 140, 200]; // Updated radii of the orbits with more space for the inner circle
+  const orbits = [75, 130, 190];
 
   return (
     <>
       <Header />
       <div className="flex justify-center items-center h-screen">
-        {/* CentralCircle representing the black hole */}
-        <div className="relative w-16 h-16 bg-black rounded-full" style={{ margin: 'auto' }}>
-          <OrbitAnimation orbits={orbits} />
-        </div>
+        <OrbitAnimation orbits={orbits} />
       </div>
     </>
   );
