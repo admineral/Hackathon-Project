8657f888a083c2c65262ee0f59e335d52a3d1b5a
admineral <50579369+admineral@users.noreply.github.com>
2023-12-10 19:11:41 +0100

    reset again but interval changed

--- app/Orbit_Components/HoverCard.js
+++ app/Orbit_Components/HoverCard.js
@@ -7,60 +7,27 @@ export default function HoverCard({ orb }) {
   const [likes, setLikes] = useState(0); // State to keep track of likes
   const [hasLiked, setHasLiked] = useState(false); // State to keep track of whether the user has liked the orb
   const hoverCardRef = useRef(null); // Ref to the hover card
-  const [isTransitioning, setIsTransitioning] = useState(false); // State to track if the hover card is transitioning
 
   const handleClick = (e) => {
     if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
       setActiveOrb(orb);
-      const clickPosition = e.clientX;
-      if (clickPosition > window.innerWidth / 2) {
-        hoverCardRef.current.style.left = 'auto';
-        hoverCardRef.current.style.right = 0;
-      } else {
-        hoverCardRef.current.style.right = 'auto';
-        hoverCardRef.current.style.left = 0;
-      }
-      setIsTransitioning(false);
     }
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // If the hover card is transitioning, don't check its position
-      if (isTransitioning) return;
-
       // Continuously check the position of the hover card and adjust its position if it's outside of the screen
       const hoverCardPosition = hoverCardRef.current.getBoundingClientRect();
-      const bufferZone = window.innerWidth * 0.1; // 10% of the screen width
-      const middleZoneStart = window.innerWidth * 0.35; // 35% of the screen width
-      const middleZoneEnd = window.innerWidth * 0.65; // 65% of the screen width
-
-      // If the hover card is within the middle zone, pause the continuous checking
-      if (hoverCardPosition.left > middleZoneStart && hoverCardPosition.right < middleZoneEnd) {
-        return;
-      }
-
-      if (hoverCardPosition.right > window.innerWidth - bufferZone) {
+      if (hoverCardPosition.right > window.innerWidth) {
         hoverCardRef.current.style.transform = `translateX(-100%)`;
-        setIsTransitioning(true);
-      } else if (hoverCardPosition.left < bufferZone) {
+      } else if (hoverCardPosition.left < 0) {
         hoverCardRef.current.style.transform = `translateX(0)`;
-        setIsTransitioning(true);
       }
-    }, 100); // Check the position every 100ms
+    }, 10000); // Check the position every 100ms
 
     // Clean up the interval when the component is unmounted
     return () => clearInterval(interval);
-  }, [isTransitioning]);
-
-  // After the transition duration, allow position checking again
-  useEffect(() => {
-    if (isTransitioning) {
-      setTimeout(() => {
-        setIsTransitioning(false);
-      }, 10000); // Transition duration
-    }
-  }, [isTransitioning]);
+  }, []);
 
   const handleLike = () => {
     if (!hasLiked) { // If the user hasn't liked the orb yet
@@ -72,8 +39,8 @@ export default function HoverCard({ orb }) {
   return (
     <div 
       ref={hoverCardRef}
-      className={`absolute w-64 p-4 bg-white rounded-lg shadow-2xl hover:scale-105`} 
-      style={{ zIndex: 1000, transition: 'transform 10s', transform: isTransitioning ? 'translateX(-100%)' : 'translateX(0)' }}
+      className={`absolute w-64 p-4 bg-white rounded-lg shadow-2xl transform transition-transform duration-500 hover:scale-105`} 
+      style={{ zIndex: 1000 }}
       onClick={handleClick}
     >
       {(activeOrb === null || activeOrb === orb) && (
@@ -83,7 +50,7 @@ export default function HoverCard({ orb }) {
           <div className="flex justify-end mt-4">
             <div className="flex items-center mr-4">
               <FaComment className="mr-2" />
-              <span className="text-gray-500">{orb.comments || 10}</span> 
+              <span className="text-gray-500">{orb.comments || 10}</span> {/* Fictive number of comments */}
             </div>
             <div className="flex items-center">
               <FaThumbsUp className="mr-2" onClick={handleLike} />
