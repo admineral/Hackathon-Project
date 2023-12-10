c73fe4af32b23185d3883edf82dc13e2a6544853
admineral <50579369+admineral@users.noreply.github.com>
2023-12-10 15:28:29 +0100

    try to fix the position hovercard new approach#1

--- app/Orbit_Components/HoverCard.js
+++ app/Orbit_Components/HoverCard.js
@@ -1,5 +1,5 @@
 // HoverCard.js
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 import { FaComment, FaThumbsUp } from 'react-icons/fa'; // Import Font Awesome icons
 
 export default function HoverCard({ orb }) {
@@ -12,16 +12,18 @@ export default function HoverCard({ orb }) {
   const handleClick = (e) => {
     if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
       setActiveOrb(orb);
-      // Check the position of the orb and adjust the position of the hover card accordingly
-      const orbPosition = e.target.getBoundingClientRect();
-      const hoverCardWidth = hoverCardRef.current.offsetWidth;
-      if (orbPosition.left + orbPosition.width + hoverCardWidth > window.innerWidth) {
-        setPosition('hover-card-left');
-      } else {
-        setPosition('hover-card-right');
-      }
     }
   };
+
+  useLayoutEffect(() => {
+    // Check the position of the hover card and adjust its position if it's outside of the screen
+    const hoverCardPosition = hoverCardRef.current.getBoundingClientRect();
+    if (hoverCardPosition.right > window.innerWidth) {
+      setPosition('hover-card-left');
+    } else if (hoverCardPosition.left < 0) {
+      setPosition('hover-card-right');
+    }
+  }, [activeOrb]);
 
   const handleLike = () => {
     if (!hasLiked) { // If the user hasn't liked the orb yet
