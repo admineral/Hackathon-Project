9919ead6881bbc78e9dc960778f1fcf821e27029
admineral <50579369+admineral@users.noreply.github.com>
2023-12-10 19:16:08 +0100

    origial working one?

--- app/Orbit_Components/HoverCard.js
+++ app/Orbit_Components/HoverCard.js
@@ -23,7 +23,7 @@ export default function HoverCard({ orb }) {
       } else if (hoverCardPosition.left < 0) {
         hoverCardRef.current.style.transform = `translateX(0)`;
       }
-    }, 10000); // Check the position every 100ms
+    }, 100); // Check the position every 100ms
 
     // Clean up the interval when the component is unmounted
     return () => clearInterval(interval);
