50d938252cd0062e9bcdb12f516ee132cb0a2aac
admineral <50579369+admineral@users.noreply.github.com>
2023-12-10 04:09:19 +0100

    mod orbsData

--- app/Orbit_Components/orbsData.js
+++ app/Orbit_Components/orbsData.js
@@ -6,9 +6,7 @@ export default [
       { size: '10', headline: 'Orbit 1 Orb 3', text: 'This is the text for orbit 1 orb 3.' },
       { size: '10', headline: 'Orbit 1 Orb 4', text: 'This is the text for orbit 1 orb 4.' },
       { size: '10', headline: 'Orbit 1 Orb 5', text: 'This is the text for orbit 1 orb 5.' },
-      { size: '10', headline: 'Orbit 1 Orb 6', text: 'This is the text for orbit 1 orb 6.' },
-      { size: '10', headline: 'Orbit 1 Orb 7', text: 'This is the text for orbit 1 orb 7.' },
-      { size: '10', headline: 'Orbit 1 Orb 8', text: 'This is the text for orbit 1 orb 8.' },
+      
     ],
     [
       { size: '20', headline: 'Orbit 2 Orb 1', text: 'This is the text for orbit 2 orb 1.' },
@@ -29,7 +27,7 @@ export default [
       { size: '38', headline: 'Orbit 3 Orb 6', text: 'This is the text for orbit 3 orb 6.' },
       { size: '20', headline: 'Orbit 3 Orb 7', text: 'This is the text for orbit 3 orb 7.' },
       { size: '27', headline: 'Orbit 3 Orb 8', text: 'This is the text for orbit 3 orb 8.' },
-      { size: '31', headline: 'Orbit 3 Orb 9', text: 'This is the text for orbit 3 orb 9.' },
+      { size: '42', headline: 'Orbit 3 Orb 9', text: 'This is the text for orbit 3 orb 9.' },
       { size: '26', headline: 'Orbit 3 Orb 10', text: 'This is the text for orbit 3 orb 10.' },
       { size: '33', headline: 'Orbit 3 Orb 11', text: 'This is the text for orbit 3 orb 11.' },
       
