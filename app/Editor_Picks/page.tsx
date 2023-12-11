c91947bec18dc91100181f5bd08764e8b6f592cb
admineral <50579369+admineral@users.noreply.github.com>
2023-12-11 21:41:45 +0100

    added Editor_Picks

--- app/Editor_Picks/page.tsx
+++ app/Editor_Picks/page.tsx
@@ -75,28 +75,28 @@ const articles = [
             </h2>
             <div className="flex overflow-x-auto space-x-4 pb-4">
             {articles.map((article, index) => (
-                <div key={article.id} className={`flex-none w-1/2 sm:w-1/4 md:w-80 rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white ${index !== articles.length - 1 ? 'sm:mr-4' : ''} border border-gray-500`}>
-                  <div className="relative">
-                    <Image alt="Article image" className="rounded-t-lg" src={article.image} width={320} height={180} layout="responsive" />
-                  </div>
-                  <div className="p-4">
-                    <h3 className="text-yellow-500 mt-2">{article.title}</h3>
-                    <p className="text-gray-400 text-sm">{article.description}</p>
-                    <div className="flex items-center justify-between mt-2">
-                      <p className="text-gray-400 text-xs">{article.author}</p>
-                      <p className="text-gray-400 text-xs">{article.date}</p>
-                    </div>
-                    <div className="flex items-center mt-2">
-                      <button className="text-gray-400 mr-2">
-                        <FaComment /> {article.comments}
-                      </button>
-                      <button className="text-gray-400">
-                        <FaHeart/> {article.likes}
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ))}
+    <div key={article.id} className={`flex-none w-full sm:w-1/2 md:w-1/4 lg:w-80 rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white ${index !== articles.length - 1 ? 'sm:mr-4' : ''} border border-gray-500`}>
+      <div className="relative">
+        <Image alt="Article image" className="rounded-t-lg" src={article.image} width={320} height={180} layout="responsive" />
+      </div>
+      <div className="p-4">
+        <h3 className="text-yellow-500 mt-2">{article.title}</h3>
+        <p className="text-gray-400 text-sm">{article.description}</p>
+        <div className="flex items-center justify-between mt-2">
+          <p className="text-gray-400 text-xs">{article.author}</p>
+          <p className="text-gray-400 text-xs">{article.date}</p>
+        </div>
+        <div className="flex items-center mt-2">
+          <button className="text-gray-400 mr-2">
+            <FaComment /> {article.comments}
+          </button>
+          <button className="text-gray-400">
+            <FaHeart/> {article.likes}
+          </button>
+        </div>
+      </div>
+    </div>
+))}
             </div>
           </div>
         </div>
