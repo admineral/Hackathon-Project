fd0bcefcff874295a330aa941f5ac57afb1df14b
admineral <50579369+admineral@users.noreply.github.com>
2023-12-12 01:04:15 +0100

    Boost Button Editor Picks working

--- app/Editor_Picks/page.tsx
+++ app/Editor_Picks/page.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client"
-
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from "next/image";
-import { FaComment } from 'react-icons/fa';
 import { IoRocketOutline } from "react-icons/io5";
 import { articles } from './articlesData';
 import { BiCommentDetail } from "react-icons/bi";
@@ -19,46 +18,59 @@ export default function EditorPicks() {
       </Head>
       <div className="text-white">
         <div className="container mx-auto px-4">
-          <h2 className="text-2xl font-bold my-4">
+          <h2 className="text-2xl font-bold my-4 text-black">
             Editor's Picks &gt;
           </h2>
           <div className="flex overflow-x-auto space-x-4 pb-4">
-            {articles.map((article, index) => (
-              <div key={article.id} className="flex-shrink-0 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-80 rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white border border-gray-500 mx-2 my-2">
-                <div className="relative">
-                  <Image
-                    alt="Article image"
-                    className="rounded-t-lg"
-                    src={article.image}
-                    width={320}
-                    height={180}
-                    priority
-                    sizes="100vw"
-                    style={{
-                      width: "100%",
-                      height: "auto"
-                    }} />
-                </div>
-                <div className="p-4">
-                  <h3 className="text-yellow-500 mt-2">{article.title}</h3>
-                  <p className="text-gray-400 text-sm">{article.description}</p>
-                  <div className="flex items-center justify-between mt-2">
-                    <p className="text-gray-400 text-xs">{article.author}</p>
-                    <p className="text-gray-400 text-xs">{article.date}</p>
+            {articles.map((article, index) => {
+              const [isRocketClicked, setIsRocketClicked] = useState(false);
+              const [likes, setLikes] = useState(article.likes);
+
+              const handleRocketClick = () => {
+                setIsRocketClicked(!isRocketClicked);
+                setLikes(likes + 1);
+              };
+
+              return (
+                <div key={article.id} className="flex-shrink-0 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-80 rounded-lg overflow-hidden shadow-lg bg-gray-800 text-white border border-gray-500 mx-2 my-2">
+                  <div className="relative">
+                    <Image
+                      alt="Article image"
+                      className="rounded-t-lg"
+                      src={article.image}
+                      width={320}
+                      height={180}
+                      priority
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "auto"
+                      }} />
                   </div>
-                  <div className="flex justify-between mt-4">
-  <button className="text-gray-400 flex items-center">
-    <BiCommentDetail />
-    <span className="ml-2">{article.comments}</span>
-  </button>
-  <button className="text-gray-400 border border-gray-400 rounded-lg px-3 py-1 flex items-center">
-    <IoRocketOutline/>
-    <span className="ml-2">{article.likes}</span>
-  </button>
-</div>
+                  <div className="p-4">
+                    <h3 className="text-yellow-500 mt-2">{article.title}</h3>
+                    <p className="text-gray-400 text-sm">{article.description}</p>
+                    <div className="flex items-center justify-between mt-2">
+                      <p className="text-gray-400 text-xs">{article.author}</p>
+                      <p className="text-gray-400 text-xs">{article.date}</p>
+                    </div>
+                    <div className="flex justify-between mt-4">
+                      <button className="text-gray-400 flex items-center">
+                        <BiCommentDetail />
+                        <span className="ml-2">{article.comments}</span>
+                      </button>
+                      <button 
+                        className={`border border-gray-400 rounded-lg px-3 py-1 flex items-center ${isRocketClicked ? 'bg-blue-300 text-black' : 'text-gray-400'}`}
+                        onClick={handleRocketClick}
+                      >
+                        <IoRocketOutline/>
+                        <span className="ml-2">{likes}</span>
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
