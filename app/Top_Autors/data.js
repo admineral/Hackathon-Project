1a709a61fc93117645a40bc84f6148872f13ebb4
admineral <50579369+admineral@users.noreply.github.com>
2023-12-13 13:00:17 +0100

    top authors

---
+++ app/Top_Autors/data.js
@@ -0,0 +1,49 @@
+export const badges = [
+    { id: 1, name: 'Krone', color: 'bg-red-600' },
+    { id: 2, name: 'Journalist', color: 'bg-blue-400' },
+    { id: 3, name: 'Contributor', color: 'bg-purple-400' },
+    { id: 4, name: 'Premium', color: 'bg-yellow-400' },
+];
+
+export const authors_gesamt = [
+    { name: 'Krone', score: 100000, followers: 500000, image: 'https://placehold.co/50x50', premium: true, badgeId: 1 },
+    { name: 'Peter Pilz', score: 50000, followers: 250000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Armin Wolf', score: 92044, followers: 392074, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Ingrid Thurnher', score: 86454, followers: 335143, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Corinna Milborn', score: 79894, followers: 323685, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Lou Lorenz-Dittlbacher', score: 84502, followers: 276624, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Tarek Leitner', score: 72348, followers: 244949, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Nadja Bernhard', score: 38443, followers: 238606, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+];
+
+export const authors_heute = [
+    { name: 'Peter Pilz', score: 50000, followers: 250000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Max Mustermann', score: 30000, followers: 150000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Anna Schmidt', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Johannes Bauer', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Julia Müller', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Stefan Schneider', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Maria Becker', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Thomas Hoffmann', score: 30000, followers: 150000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+];
+export const authors_woche = [
+    { name: 'Peter Pilz', score: 50000, followers: 250000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Lukas Lehner', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Sophie Wagner', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Martin Maier', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Laura Lang', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Daniel Dorn', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Julia Jäger', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Stefan Stein', score: 30000, followers: 150000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+];
+
+export const authors_monat = [
+    { name: 'Peter Pilz', score: 50000, followers: 250000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Max Müller', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Anna Altmann', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Johannes Jung', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Julia Jäger', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Stefan Stein', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+    { name: 'Maria Maier', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
+    { name: 'Thomas Thaler', score: 30000, followers: 150000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+];
--- app/Top_Autors/page.tsx
+++ app/Top_Autors/page.tsx
@@ -1,40 +1,69 @@
-import React from 'react';
+"use client"
 
-const authors = [
-    { name: 'EXCAVO', score: 92044, followers: 392074, image: 'https://placehold.co/50x50', premium: true },
-    { name: 'tntsunrise', score: 86454, followers: 335143, image: 'https://placehold.co/50x50', premium: true },
-    { name: 'DLavrov', score: 79894, followers: 323685, image: 'https://placehold.co/50x50', premium: true },
-    { name: 'alanmasters', score: 84502, followers: 276624, image: 'https://placehold.co/50x50', premium: true },
-    { name: 'MagicPoopCannon', score: 72348, followers: 244949, image: 'https://placehold.co/50x50', premium: true },
-    { name: 'TomHall', score: 38443, followers: 238606, image: 'https://placehold.co/50x50', premium: true },
-];
+import React, { useState } from 'react';
+import { badges, authors_gesamt, authors_heute, authors_woche, authors_monat } from './data';
+
+
 
 function App() {
+    const [selectedTime, setSelectedTime] = useState('All time');
+    const [displayedAuthors, setDisplayedAuthors] = useState(authors_gesamt);
+
+    const handleTimeSelect = (option: string) => {
+        setSelectedTime(option);
+        switch (option) {
+            case 'Heute':
+                setDisplayedAuthors(authors_heute);
+                break;
+            case 'Woche':
+                setDisplayedAuthors(authors_woche);
+                break;
+            case 'Monat':
+                setDisplayedAuthors(authors_monat);
+                break;
+            default:
+                setDisplayedAuthors(authors_gesamt);
+        }
+    };
+
+    const timeOptions = ['Heute', 'Woche', 'Monat', 'Gesamt'];
+
     return (
         <div className="bg-gray-800 text-white p-8 rounded-lg">
             <h2 className="text-2xl font-semibold mb-4">Krone insights</h2>
             <div className="bg-gray-900 p-4 rounded-lg">
-                <h3 className="text-xl font-semibold mb-2">Top authors</h3>
+                <h3 className="text-xl font-semibold mb-4">Top Autoren</h3>
                 <div className="text-blue-400 mb-4">
-                    <span className="mr-2">Today</span>
-                    <span className="mr-2">Week</span>
-                    <span className="mr-2">Month</span>
-                    <span className="font-semibold">All time</span>
+                {timeOptions.map(option => (
+                    <span
+                        key={option}
+                        className={`mr-2 cursor-pointer px-2 py-1 rounded ${selectedTime === option ? 'bg-blue-400 text-white border-transparent' : 'border border-blue-400 text-blue-400'} hover:bg-blue-400 hover:text-white hover:border-transparent`}
+                        onClick={() => handleTimeSelect(option)}
+                    >
+                        {option}
+                    </span>
+                ))}
                 </div>
-                {authors.map((author, index) => (
-                    <div key={index} className="flex items-center justify-between bg-gray-700 p-3 mb-2 rounded-lg">
-                        <div className="flex items-center">
+                {displayedAuthors.map((author, index) => {
+                    const badge = badges.find(badge => badge.id === author.badgeId);
+                    return (
+                        <div key={index} className="flex items-center justify-between bg-gray-700 p-4 mb-2 rounded-lg">
+                            <div className="flex items-center">
                             <img src={author.image} alt={`Profile picture of ${author.name}`} className="rounded-full w-10 h-10 mr-3" />
-                            <div>
-                                <div className="font-semibold">{author.name} {author.premium && <span className="bg-yellow-400 text-gray-900 text-xs px-0.5 py-0 rounded-full ml-2">PREMIUM</span>}</div>
-                                <div className="text-gray-400 text-sm">
-                                    <i className="fas fa-user mr-1"></i> {author.score.toLocaleString()} <i className="fas fa-users ml-3 mr-1"></i> {author.followers.toLocaleString()}
+                                <div className="flex flex-col flex-grow">
+                                    <div className="flex items-center justify-between">
+                                        <div className="font-semibold truncate">{author.name}</div>
+                                        {badge && <span className={`font-sans ${badge.color} text-gray-900 text-xs px-1.5 py-0.5 rounded-full ml-1 mr-3 flex-shrink-0`}>{badge.name}</span>}
+                                    </div>
+                                    <div className="text-gray-400 text-sm truncate">
+                                        <i className="fas fa-user mr-1"></i> {author.score.toLocaleString()} <i className="fas fa-users ml-3 mr-1"></i> {author.followers.toLocaleString()}
+                                    </div>
                                 </div>
                             </div>
+                            <button className="text-blue-400 border border-blue-400 rounded-lg px-4 py-1 hover:bg-blue-400 hover:text-white transition-colors duration-200 flex-shrink-0">Follow</button>
                         </div>
-                        <button className="text-blue-400 border border-blue-400 rounded-lg px-4 py-1 hover:bg-blue-400 hover:text-white transition-colors duration-200">Follow</button>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
