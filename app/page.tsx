a9f82b777a8012d623d95b4608c8cf4bbb005836
admineral <50579369+admineral@users.noreply.github.com>
2023-12-13 13:43:04 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,18 @@ import TopAuthors from './Top_Autors/page';
 export default function Home() {
   const orbits = [55, 110, 180];
 
-  return (
-    <>
-      <Header />
-      <div className="flex justify-center items-center h-screen">
-        <OrbitAnimation orbits={orbits} orbsData={orbsData} />
-      </div>
-      <EditorPicks />
-      <div style={{ display: 'flex', justifyContent: 'flex-start' }}> {/* Add a div with flex and justifyContent: 'flex-start' */}
-        <TopAuthors />
-      </div>
-      <RoadmapList />
-    </>
-  );
+// app/page.tsx
+return (
+  <>
+    <Header />
+    <div className="flex justify-center items-center h-screen">
+      <OrbitAnimation orbits={orbits} orbsData={orbsData} />
+    </div>
+    <EditorPicks />
+    <div style={{ display: 'flex', alignSelf: 'flex-start', marginLeft: '150px' }}> {/* Add marginLeft: '20px' */}
+      <TopAuthors />
+    </div>
+    <RoadmapList />
+  </>
+);
 }
