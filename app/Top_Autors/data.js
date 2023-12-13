0ff88850d1de6252ce0010959fd87d2c42922d38
admineral <50579369+admineral@users.noreply.github.com>
2023-12-13 13:48:37 +0100

    update

--- app/Top_Autors/data.js
+++ app/Top_Autors/data.js
@@ -8,10 +8,9 @@ export const badges = [
 export const authors_gesamt = [
     { name: 'Krone', score: 100000, followers: 500000, image: '/Krone_Profile.png', premium: true, badgeId: 1 },
     { name: 'Peter Pilz', score: 50000, followers: 250000, image: '/peter.png', premium: true, badgeId: 4, link: '/profile/peter-pilz' },
-    { name: 'Armin Wolf', score: 92044, followers: 392074, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
-    { name: 'Ingrid Thurnher', score: 86454, followers: 335143, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
-    { name: 'Corinna Milborn', score: 79894, followers: 323685, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
-    { name: 'Lorenz-Dittlbacher', score: 84502, followers: 276624, image: 'https://placehold.co/50x50', premium: true, badgeId: 2 },
+    { name: 'Armin Wolf', score: 92044, followers: 392074, image: '/wolf.png', premium: true, badgeId: 2 },
+    { name: 'Ingrid Thurnher', score: 86454, followers: 335143, image: '/thurn.png', premium: true, badgeId: 2 },
+
  
 ];
 
@@ -20,8 +19,7 @@ export const authors_heute = [
     { name: 'Max Mustermann', score: 30000, followers: 150000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
     { name: 'Anna Schmidt', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
     { name: 'Johannes Bauer', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
-    { name: 'Julia Müller', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
-    { name: 'Stefan Schneider', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+
  
 ];
 export const authors_woche = [
@@ -29,8 +27,7 @@ export const authors_woche = [
     { name: 'Lukas Lehner', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
     { name: 'Sophie Wagner', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
     { name: 'Martin Maier', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
-    { name: 'Laura Lang', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
-    { name: 'Daniel Dorn', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+   
  
 ];
 
@@ -39,7 +36,6 @@ export const authors_monat = [
     { name: 'Max Müller', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
     { name: 'Anna Altmann', score: 45000, followers: 225000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
     { name: 'Johannes Jung', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
-    { name: 'Julia Jäger', score: 40000, followers: 200000, image: 'https://placehold.co/50x50', premium: true, badgeId: 4 },
-    { name: 'Stefan Stein', score: 35000, followers: 175000, image: 'https://placehold.co/50x50', premium: false, badgeId: 3 },
+
  
 ];
