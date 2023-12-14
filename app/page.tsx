fbb2c4739f59fadbfeb751dad4b1464740f4f3a7
admineral <50579369+admineral@users.noreply.github.com>
2023-12-14 22:38:41 +0100

    fixed author page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
 // app/page.tsx
 "use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../components/layout/Header';
 import OrbitAnimation from './Orbit_Components/OrbitAnimation';
 import orbsData from './Orbit_Components/orbsData';
 import RoadmapList from './Reddit_Posts/page';
 import EditorPicks from './Editor_Picks/page'; 
 import TopAuthors from './Top_Autors/page';
+import useMediaQuery from '../lib/hooks/use-media-query'; // import the hook
 
 export default function Home() {
   const orbits = [55, 110, 180];
-  const [isMobile, setIsMobile] = useState(typeof window !== 'undefined' ? window.innerWidth <= 768 : false);
-  
-
-  useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth <= 768);
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
+  const { isMobile } = useMediaQuery(); // use the hook
 
   return (
     <>
@@ -27,7 +20,7 @@ export default function Home() {
         <OrbitAnimation orbits={orbits} orbsData={orbsData} />
       </div>
       <EditorPicks />
-      <div style={{ display: 'flex', alignSelf: 'flex-start', marginLeft: isMobile ? '0px' : '150px' }}>
+      <div style={{ display: 'flex', alignSelf: 'flex-start', marginLeft: isMobile ? '0px' : '100px' }}>
         <TopAuthors />
       </div>
       <RoadmapList />
