7149d3e68230a78c56f1565b84d1b04624a2c96e
admineral <50579369+admineral@users.noreply.github.com>
2023-12-14 20:27:02 +0100

    final fix?

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
-import NextAuth, { SessionStrategy } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  session: {
-    strategy: 'jwt' as SessionStrategy,
-  },
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
-};
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import { authOptions } from "../../../auth/auth-config";
 
 const handler = NextAuth(authOptions);
 
---
+++ app/auth/auth-config.ts
@@ -0,0 +1,21 @@
+// auth-config.ts
+import NextAuth, { SessionStrategy } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  session: {
+    strategy: 'jwt' as SessionStrategy,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
