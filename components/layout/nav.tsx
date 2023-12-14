1bb6b4745d6b338d668084d6c4863ddf3a49c7f3
admineral <50579369+admineral@users.noreply.github.com>
2023-12-14 20:37:06 +0100

    final fix?

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,6 +1,7 @@
+// components/layout/nav.tsx
 import Navbar from "./navbar";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../../app/auth/auth-config";
 
 export default async function Nav() {
   const session = await getServerSession(authOptions);
