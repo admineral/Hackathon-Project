09328ebb562f1a139ed762ca9f935ed86e7d16d5
admineral <50579369+admineral@users.noreply.github.com>
2023-12-18 18:22:18 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <div className="w-full md:w-1/3" style={{ minWidth: '200px' }}>
             <TopAuthors />
           </div>
-          <div className="w-full md:flex-grow" style={{ maxWidth: 'calc(100% - 450px)' }}>
+          <div className="w-full md:flex-grow">
             <EditorPicks />
           </div>
         </div>
