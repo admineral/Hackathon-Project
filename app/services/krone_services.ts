396d4b3bc18886fffd2f1acdd320aac625b6649a
admineral <50579369+admineral@users.noreply.github.com>
2024-01-31 19:06:52 +0100

    testing vercel cronjobs

--- app/services/krone_services.ts
+++ app/services/krone_services.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 import { parseRssData } from './krone_rss_parser';
 
 export async function parseRssFeed() {
-  const response = await fetch('http://localhost:3000/api/get_krone_rss');
+  const response = await fetch('https://www.kronews.at/api/get_krone_rss');
   const { data } = await response.json();
 
   const { articles, final_articles } = await parseRssData(data);
