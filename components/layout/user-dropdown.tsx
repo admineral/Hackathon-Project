ddc511d0aef20aba178cbe25a229ca97fefdd148
admineral <50579369+admineral@users.noreply.github.com>
2024-03-12 21:38:30 +0100

    bug fix

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -30,7 +30,7 @@ export default function UserDropdown({ session }: { session: Session }) {
                 {session?.user?.email}
               </p>
             </div>
-            <Link href="/orbit_components">
+            <Link href="/Orbit_Components">
               <a className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100">
                 <LayoutDashboard className="h-4 w-4" />
                 <p className="text-sm">Kroniversum</p>
